fd803b42ac4675993f1b81ef7b9a181c6ec85441
baker <bakeronx@duck.com>
2024-04-08 15:16:26 +0400

    login and register page

--- pages/router.py
+++ pages/router.py
@@ -18,3 +18,12 @@ async def get_location_page(request: Request):
 @router.get("/catalog")
 async def get_catalog_page(request: Request):
     return templates.TemplateResponse("catalog.html", {"request": request})
+
+@router.get("/register")
+async def get_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@router.get("/login")
+async def get_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
